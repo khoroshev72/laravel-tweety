10aab2d654874c03c453a61b652317360c36b172
Eugene <khoroshev72@mail.ru>
2021-02-14 15:33:47 +0500

    dynamic timeline page

---
+++ app/Http/Controllers/TweetController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tweet;
+use Illuminate\Http\Request;
+
+class TweetController extends Controller
+{
+    public function store()
+    {
+        $attrs = request()->validate([
+            'body' => 'required|max:255'
+        ]);
+
+        $attrs['user_id'] = auth()->id();
+
+        Tweet::create($attrs);
+
+        return redirect()->back();
+    }
+}
---
+++ database/migrations/2021_02_13_134700_create_tweets_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTweetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tweets', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->text('body');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tweets', function (Blueprint $table) {
+            $table->dropForeign('tweets_user_id_foreign');
+            $table->dropIfExists();
+        });
+    }
+}
